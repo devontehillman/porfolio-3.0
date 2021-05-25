c86715eba00bccfc2eeee44fce00f7eda28c1cbe
devontehillman <hillmand@mail.gvsu.edu>
2021-05-25 14:31:08 -0400

    added stying for nav

---
+++ portfolio3.0/src/components/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div className="content-section">
+                <h1>Devonte Hillman</h1>
+                <p>My desire to program started in my first engineering class in college when we used C to create a self scoring Arduino robot. Since then I have attended a bootcamp to lean Full stack web development. In the future I plan to use my knowledge to work in the industry of machine learning and the internet of things because I have a strong desire to use computer programming to help automate everyday processes.</p>
+            </div>
+        )
+    }
+}
